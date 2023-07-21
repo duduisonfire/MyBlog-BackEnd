8a32616edea3d7d7b073e938a2e5d755c639cbba
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-07-21 19:59:12 -0300

    Fixed an bug on AddTransient to Post Services. rs'

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(
 );
 
 builder.Services.AddTransient<IBlogPostRepository, BlogPostRepository>();
-builder.Services.AddTransient<IBlogPostServices, IBlogPostServices>();
+builder.Services.AddTransient<IBlogPostServices, BlogPostServices>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
